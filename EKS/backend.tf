9622a8735658b6fc8337e35dbeb85a4b085ccc67
Shakthikm <91306193+Shakthikm@users.noreply.github.com>
2026-01-22 22:13:33 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "Shakthi-project-s3bucket"  # Replace with your actual S3 bucket name
+    bucket = "shakthi-project-s3bucket"  # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"  
     region = "ap-southeast-2" 
   }
